98c5ca755ee1294715d3aa66433bd8158c8c14f2
mdsajjadhossain25 <md.sajjad.hossain.025@gmail.com>
2023-09-20 18:23:38 +0600

    account activation and email verification setup complete

--- ecommerce/accounts/views.py
+++ ecommerce/accounts/views.py
@@ -1,8 +1,17 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, render, HttpResponse
 
 from accounts.models import Account
 from .forms import RegistrationForm
-from django.contrib import messages
+from django.contrib import messages, auth
+from django.contrib.auth.decorators import login_required
+
+# verification email
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import EmailMessage
 
 # Create your views here.
 
@@ -23,8 +32,22 @@ def register(request):
                 first_name=first_name, last_name=last_name, email=email, username=username, password=password)
             user.phone_number = phone_number
             user.save()
-            messages.success(request, 'Registration Successful')
-            return redirect('register')
+            
+            # user activation 
+            current_site = get_current_site(request)
+            mail_subject = "Please Activate Your Account"
+            message = render_to_string('accounts/account_verification_email.html', {
+                'user':user,
+                'domain':current_site,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': default_token_generator.make_token(user),
+            })
+            to_email = email
+            send_email = EmailMessage(mail_subject, message, to=[to_email])
+            send_email.send()
+            # messages.success(request, 'Thank your for registering. We have send you a verification email to your email address. Please verify your account!')
+            
+            return redirect('/accounts/login/?command=verification&email='+email)
     else:
         form = RegistrationForm()
 
@@ -35,8 +58,39 @@ def register(request):
 
 
 def login(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+        user = auth.authenticate(email=email, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            # messages.success(request, 'You are now logged in.')
+            return redirect('home')
+        else:
+            messages.error(request, 'Invalid login credentials')
+            return redirect('login')
     return render(request, 'accounts/login.html')
 
 
+@login_required(login_url='login')
 def logout(request):
-    return
+    auth.logout(request)
+    messages.success(request, 'You are logged out!')
+    return redirect('login')
+
+def activate(request, uidb64, token):
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = Account._default_manager.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, Account.DoesNotExist):
+        user = None
+        
+    if user is not None and default_token_generator.check_token(user, token):
+        user.is_active=True
+        user.save()
+        messages.success(request, 'Your account is activated!')
+        return redirect('login')
+    else:
+        messages.error(request, 'Invalid activation link')
+        return redirect('register')
--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -1,5 +1,6 @@
 
 
+from django.contrib.messages import constants as messages
 import os
 from pathlib import Path
 
@@ -130,8 +131,15 @@ MEDIA_ROOT = BASE_DIR/'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-from django.contrib.messages import constants as messages
 
 MESSAGE_TAGS = {
     messages.ERROR: "danger",
 }
+
+
+# smtp configuration
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'hmdsajjad840@@gmail.com'
+EMAIL_HOST_PASSWORD = 'xpzi czwj wair ietd'
+EMAIL_USE_TLS = True

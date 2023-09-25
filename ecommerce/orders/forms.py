1c35b1c1d60beb7d15af74b7a793603a7ed68cd7
mdsajjadhossain25 <md.sajjad.hossain.025@gmail.com>
2023-09-26 01:42:09 +0600

    order page setup complete

--- ecommerce/orders/forms.py
+++ ecommerce/orders/forms.py
@@ -1,8 +1,11 @@
 from django import forms
 from .models import Order
 
-# code goes here 
+# code goes here
+
+
 class OrderForm(forms.ModelForm):
     class Meta:
-        models = Order
-        fields = ['first_name', 'last_name', 'phone', 'email', 'address_line_1', 'address_line_2', 'country', 'state', 'city', 'order_note']
+        model = Order
+        fields = ['first_name', 'last_name', 'phone', 'email', 'address_line_1',
+                  'address_line_2', 'country', 'state', 'city', 'order_note']
